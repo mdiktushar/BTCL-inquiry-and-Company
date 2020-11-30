e79148ce154d7d34bcc62bfa83309e6812f9fa7c
mdiktushar <md.tushar1804@gmail.com>
2020-12-01 00:35:53 +0600

    commit navbar-1 01/12/20

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-<H1>This project is based on BTCL company.</H1>
+<H1>BTCL inquiry and Company<H1>
+<H2>This project is based on BTCL company.</H2>
 <p>
-BTCL or Bangladesh Telecommunications Company Limited is the largest telecommunications company in Bangladesh. The company was founded as the Bangladesh Telegraph & Telephone Board (BTTB) following Bangladesh's independence in 1971.
-This project is going to help customers to report what problem that they are having with the connection and help company to assign workers to that problem so company can inquire that problem immediately.
+    BTCL or Bangladesh Telecommunications Company Limited is the largest telecommunications company in Bangladesh. The company was founded as the Bangladesh Telegraph & Telephone Board (BTTB) following Bangladesh's independence in 1971.
+</p>
+<p>
+    This project is going to help customers to report what problem that they are having with the connection and help company to assign workers to that problem so company can inquire that problem immediately.
 </p>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BTCL Help line</title>
+
+        <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/nav.css">
+
+    <!-- jQuery and JS bundle w/ Popper.js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+
+
+</head>
+<body>
+    <nav class="navbar navbar-light bg-light navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+        <img src="image/Logo.jpg" width="60" height="30" alt="" loading="lazy">
+        </a>
+        <a class="navbar-brand">Welcome to Customer service</a>
+        <form class="form-inline">
+            <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
+            <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+            <!-- <button class="btn  my-2 my-sm-0 navLogin" type="submit" >login</button> -->
+            <button type="button" class="btn btn-light">Login</button>
+        </form>
+    </nav>
+    
+    
+</body>
+</html>
