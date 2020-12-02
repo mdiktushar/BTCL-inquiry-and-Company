4ae5dd651256d0cb610ff22c69fd56c833d9ac18
mdiktushar <md.tushar1804@gmail.com>
2020-12-03 00:30:08 +0600

    commt index 03/12/2020 commit-1

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
         <!-- CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/indexStyle.css">
 
     <!-- jQuery and JS bundle w/ Popper.js -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -21,7 +21,7 @@
 <body>
     <nav class="navbar navbar-light bg-light navbar navbar-dark bg-dark">
         <a class="navbar-brand" href="#">
-        <img src="image/Logo.jpg" width="60" height="30" alt="" loading="lazy">
+        <img src="image/btcl_logo.png" width="75" height="35" alt="" loading="lazy">
         </a>
         <a class="navbar-brand">Welcome to Customer service</a>
         <form class="form-inline">
@@ -31,7 +31,43 @@
             <button type="button" class="btn btn-light">Login</button>
         </form>
     </nav>
-    
-    
+    <br><br>
+    <div class="container">
+        <h2>Our Provided Services:</h2>
+
+        <div class="row "> 
+            <div class="col-lg-4 col-6 ">
+                <img class="img-thumbnail img-fluid imgpad" src="image/Services/Telephone.png" alt="">
+                <div class="center">Telephone</div>
+            </div>
+
+            <div class="col-lg-4 col-6 ">
+                <img class="img-thumbnail img-fluid imgpad" src="image/Services/Internet.png" alt="">
+                <div class="center">Internet</div>
+            </div>
+
+            <div class="col-lg-4 col-6 ">
+                <img class="img-thumbnail img-fluid imgpad" src="image/Services/Domain & Hosting.png" alt="">
+                <div class="center">Domain &<br>Hosting</div>
+            </div>
+
+            <div class="col-lg-4 col-6 ">
+                <img class="img-thumbnail img-fluid imgpad" src="image/Services/Transmission.png" alt="">
+                <div class="center">Transmission</div>
+            </div>
+
+            <div class="col-lg-4 col-6 ">
+                <img class="img-thumbnail img-fluid imgpad" src="image/Services/Bill and Payment.png" alt="">
+                <div class="center">Bill and<br>Payment</div>
+            </div>
+
+            <div class="col-lg-4 col-6 ">
+                <img class="img-thumbnail img-fluid imgpad" src="image/Services/Complain Online.png" alt="">
+                <div class="center">Complain<br>Online</div>
+            </div>
+
+        </div>
+    </div>
+
 </body>
 </html>
