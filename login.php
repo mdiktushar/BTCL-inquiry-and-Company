7addd36eef671530feedf117ef3ff0250d45ecf5
mdiktushar <md.tushar1804@gmail.com>
2020-12-06 23:52:40 +0600

    loging successful

--- login.php
+++ login.php
@@ -1,3 +1,31 @@
+
+<?php
+
+    session_start();
+    require_once "pdo.php";
+
+    if (isset($_POST['pass']) && isset($_POST['ID'])) {
+
+        $stmt = $pdo->prepare('SELECT ID FROM customer WHERE ID = :ID AND pass = :pw');
+        
+        $stmt->execute(array(':ID' => $_POST['ID'], ':pw' => $_POST['pass']));
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($row !== false){
+            $_SESSION['ID_number'] = $row['ID'];
+
+
+            header("Location: home.php");
+            return;
+        }
+    
+    }
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -36,15 +64,15 @@
     
 
     <div class="container">
-        <form>
+        <form method="POST" action="login.php">
             <div class="form-group textStyle">
                     <label for="exampleInputEmail1">User ID</label>
-                    <input type="" class="form-control"  required maxlength="5" minlength="4" >
+                    <input type="" class="form-control"  required maxlength="5" minlength="4" name="ID" >
                     <small id="emailHelp" class="form-text text-muted">Your Telephone number is your user ID</small>
             </div>
             <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" required minlength="6">
+                    <input type="password" class="form-control" id="exampleInputPassword1" required minlength="6" name="pass">
             </div>
 
             <button type="submit" class="btn btn-primary">Login</button>
