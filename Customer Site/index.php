fac4465ffbb79cbced9516f9ac175f819d72c44e
mdiktushar <md.tushar1804@gmail.com>
2021-03-19 12:06:25 +0600

    commit-1 19/3/2021

--- Customer Site/index.php
+++ Customer Site/index.php
@@ -1,6 +1,8 @@
 <?php
     session_start();
-    $_SESSION['line_manLogin'] = 0;
+    // $_SESSION['line_manLogin'] = 0;
+    if($_SESSION['User_login']!=1)
+        $_SESSION['User_login'] = 0;
 
 ?>
 
